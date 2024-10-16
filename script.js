547d066976a8f25e55d3055046bb40a6f846b177
Ange230700 <kouakouangeericstephane@gmail.com>
2024-10-16 11:39:15 +0200

    added feature asking for user input and added an extra something for the dynamic use of sayHello()

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const firstName = prompt("Quel est votre prénom ?");
+
 function sayHello(firstName, hour) {
     let greeting = "Bonjour";
     if (hour >= 18) {
@@ -8,6 +10,5 @@ function sayHello(firstName, hour) {
     document.querySelector('h1').innerText = message;
 }
 
-sayHello(`Beyoncé`, 11); // expected: Bonjour Beyoncé !
-sayHello(`Beyoncé`, 18); // expected: Bonsoir Beyoncé !
-sayHello(`Beyoncé`, 17); // expected: Bonjour Beyoncé !
+const hour = new Date().getHours();
+sayHello(firstName, hour); // dynamic use of the function by passing the current hour with new Date().getHours()
